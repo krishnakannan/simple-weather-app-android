2a600c79fb6d63a58b6fa673d825ef93774789e7
Krish <k.krish@yahoo.com>
2016-08-18 19:00:10 +0530

    Api Changes

--- app/src/main/java/io/github/krishnakannan/simpleweatherapp/Util/AppConstants.java
+++ app/src/main/java/io/github/krishnakannan/simpleweatherapp/Util/AppConstants.java
@@ -5,11 +5,11 @@ package io.github.krishnakannan.simpleweatherapp.Util;
  */
 public class AppConstants {
 
-    public static String CURRENT_FORECAST_API_URL = "http://www.nea.gov.sg/api/WebAPI/?dataset=2hr_nowcast&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
+    public static String CURRENT_FORECAST_API_URL = "http://www.api.nea.gov.sg/api/WebAPI/?dataset=2hr_nowcast&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
 
-    public static String DAY_FORECAST_API_URL = "http://www.nea.gov.sg/api/WebAPI/?dataset=24hrs_forecast&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
+    public static String DAY_FORECAST_API_URL = "http://www.api.nea.gov.sg/api/WebAPI/?dataset=24hrs_forecast&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
 
-    public static String WEEK_FORECAST_API_URL = "http://www.nea.gov.sg/api/WebAPI/?dataset=4days_outlook&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
+    public static String WEEK_FORECAST_API_URL = "http://www.api.nea.gov.sg/api/WebAPI/?dataset=4days_outlook&keyref=781CF461BB6606AD120881175FC740619D8FB25A3EA2FF69";
 
     public static String MAPS_API = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
 
